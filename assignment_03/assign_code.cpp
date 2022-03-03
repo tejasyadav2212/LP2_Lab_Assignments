9a2e55bbb48d7cddd0f23d0cfc9e27870ffa46d2
tejasyadav2212 <70097081+tejasyadav2212@users.noreply.github.com>
2022-03-03 12:12:27 +0530

    Update assign_code.cpp

--- assignment_03/assign_code.cpp
+++ assignment_03/assign_code.cpp
@@ -1,6 +1,15 @@
 // assignment 03  
 // greedy algorithm for selection sort...
 
+// Implement Greedy search algorithm for any of the following application:
+// I. Selection Sort
+// II. Minimum Spanning Tree
+// III. Single-Source Shortest Path Problem
+// IV. Job Scheduling Problem
+// V. Prim's Minimal Spanning Tree Algorithm
+// VI. Kruskal's Minimal Spanning Tree Algorithm
+// VII. Dijkstra's Minimal Spanning Tree Algorithm
+
 #include <iostream>
 using namespace std;
 
